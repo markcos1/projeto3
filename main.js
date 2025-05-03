526f9815e86dae2620526e5b57402f30188c87ba
marcos <marcos@exemplo.com>
2025-05-02 21:52:18 -0300

    novo commit

--- main.js
+++ main.js
@@ -3,4 +3,35 @@ $(document).ready(function(){
         $('nav').slideToggle();
     });
 
+    $('#telefone').mask('(00) 00000-0000');
+
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, digite seu nome.',
+            email: 'Por favor, digite seu E-mail.',
+            telefone: 'Por favor, digite um telefone.'
+        },
+        submitHandler: function(form) {
+            e.preventDefault();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    })
 })
